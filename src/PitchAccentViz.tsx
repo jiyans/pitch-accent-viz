69f93779e11ced775659de1ac56dc2848081dbcb
Jiyan Schneider <jiyan.schneider@keio.jp>
2025-04-10 17:50:44 +0900

    fix words

--- src/PitchAccentViz.tsx
+++ src/PitchAccentViz.tsx
@@ -8,7 +8,7 @@ function splitIntoMoras(text) {
 }
 
 export function PitchAccentViz() {
-  const [inputText, setText] = useState("こんは");
+  const [inputText, setText] = useState("こんにちは");
   const [pitchLevels, setPitchLevels] = useState({});
 
   const moras = splitIntoMoras(inputText);
