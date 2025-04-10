720d7d9417490ab95d502d2af46aec735c7c3c1f
Jiyan Schneider <jiyan.schneider@keio.jp>
2025-04-10 17:35:31 +0900

    update the app

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ import reactLogo from "./react.svg";
 export function App() {
   return (
     <div className="max-w-7xl mx-auto p-8 text-center relative z-10">
-      <h1 className="text-5xl font-bold my-4 leading-tight">高低アクセントツール</h1>
-      <p>
-        下記のBox に高低アクセントを入れてみてほしい。
-      </p>
+      <h1 className="text-5xl font-bold my-4 leading-tight">
+        高低アクセントツール
+      </h1>
+      <p>カタカナを入れて、文字をクリックしてみてー</p>
       <PitchAccentViz />
     </div>
   );
