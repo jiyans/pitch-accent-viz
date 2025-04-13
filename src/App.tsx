c0ce94f3262a87f86b49c54c6beb2064536c568f
Jiyan Schneider <jiyan.schneider@keio.jp>
2025-04-13 12:59:16 +0900

    change name

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { PitchAccentViz as PitchAccentViz } from './PitchAccentViz';
 export function App() {
   return (
     <div className="max-w-7xl mx-auto p-4 text-center relative z-10">
-      <h1 className="text-4x1 sm:text-6xl font-bold my-4 leading-tight">高低アクセントツール</h1>
+      <h1 className="text-4x1 sm:text-6xl font-bold my-4 leading-tight">ダンゴネーション</h1>
       <p>カタカナを入れて、文字をクリックしてみてー</p>
       <PitchAccentViz />
     </div>
