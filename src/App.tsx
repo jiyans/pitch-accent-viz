19dbf9a202716102738cd43e9519f74ca573c4b1
Jiyan Jonas Schneider <mail@jiyanjs.com>
2025-04-24 17:49:40 +0900

    change wording

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export function App() {
   return (
     <div className="max-w-7xl mx-auto p-4 text-center relative z-10">
       <h1 className="text-4x1 sm:text-6xl font-bold my-4 leading-tight">ダンゴネーション</h1>
-      <p>カタカナを入れて、文字をクリックしてみてー</p>
+      <p>ひらがな・カタカナを入れて、文字をクリックしてみてー</p>
       <PitchAccentViz />
     </div>
   );
